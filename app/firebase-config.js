266f55bd53c4a2f62d27f2a39e9dad77e30a18b3
einar-borsheim <einar.borsheim@gmail.com>
2026-09-22 13:58:19 +0200

    Bytt til nytt Firebase-prosjekt uten organisasjonssperre

--- app/firebase-config.js
+++ app/firebase-config.js
@@ -3,10 +3,10 @@
 // Disse verdiene er IKKE hemmelige (de er ment å ligge i klientkode),
 // men tilgang til data styres av Firestore/Storage-reglene (se app/README.md).
 export const firebaseConfig = {
-  apiKey: "AIzaSyAue78OfTindZq84zXwv3L3XVoBONI2VMI",
-  authDomain: "einar-borsheim.firebaseapp.com",
-  projectId: "einar-borsheim",
-  storageBucket: "einar-borsheim.firebasestorage.app",
-  messagingSenderId: "497059476414",
-  appId: "1:497059476414:web:132d84c19fef6bd39e3fef",
+  apiKey: "AIzaSyDiQVaRLA6tmwLocEEMNYFOyBQBWHuZkzc",
+  authDomain: "einar-borsheim-internkon-ee8b6.firebaseapp.com",
+  projectId: "einar-borsheim-internkon-ee8b6",
+  storageBucket: "einar-borsheim-internkon-ee8b6.firebasestorage.app",
+  messagingSenderId: "44514761758",
+  appId: "1:44514761758:web:63586765fe8c38d13fa20f",
 };
